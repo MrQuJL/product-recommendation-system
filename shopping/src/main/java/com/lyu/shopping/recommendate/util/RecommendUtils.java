125d8d8bcab1ac526689fd8784d897b623010e3e
MrQuJL <909301740@qq.com>
2018-03-27 09:22:34 +0800

    replace tab for fout blank

--- shopping/src/main/java/com/lyu/shopping/recommendate/util/RecommendUtils.java
+++ shopping/src/main/java/com/lyu/shopping/recommendate/util/RecommendUtils.java
@@ -18,161 +18,161 @@ import com.lyu.shopping.sysmanage.entity.Product;
  * @version V1.0
  */
 public class RecommendUtils {
-	
-	/**
-	 * 更新用户的购买行为，最终插入数据库中
-	 * @param userId 用户id
-	 * @param itemId 商品id
-	 * @return true则更新成功，false则更新失败
-	 */
-	public static boolean updateBuyingBehavior(Long userId, Long itemId) {
-		boolean flag = false;
-		
-		
-		return flag;
-	}
-	
-	/**
-	 * 将用户的购买行为组装成一个map,key为userId，value也是一个map，这个map记录的是二级类目以及它对应的点击量
-	 * @param userActiveList 用户的购买行为列表
-	 * @return 组装好的用户的购买行为的map集合
-	 */
-	public static ConcurrentHashMap<Long, ConcurrentHashMap<Long, Long>> assembleUserBehavior(List<UserActiveDTO> userActiveList) {
-		ConcurrentHashMap<Long, ConcurrentHashMap<Long, Long>> activeMap = new ConcurrentHashMap<Long, ConcurrentHashMap<Long, Long>>();
-		// 遍历查询到的用户点击行为数据
-		for (UserActiveDTO userActive : userActiveList) {
-			// 1.获取用户id
-			Long userId = userActive.getUserId();
-			// 2.获取二级类目id
-			Long category2Id = userActive.getCategory2Id();
-			// 3.获取该二级类目的点击量
-			Long hits = userActive.getHits();
-			
-			// 判断activeMap中是否已经存在了该userId的信息，如果存在则进行更新
-			if (activeMap.containsKey(userId)) {
-				ConcurrentHashMap<Long, Long> tempMap = activeMap.get(userId);
-				tempMap.put(category2Id, hits);
-				activeMap.put(userId, tempMap);
-			} else {
-				// 不存在则直接put进
-				ConcurrentHashMap<Long, Long> category2Map = new ConcurrentHashMap<Long, Long>();
-				category2Map.put(category2Id, hits);
-				activeMap.put(userId, category2Map);
-			}
-		}
-		
-		return activeMap;
-	}
-	
-	/**
-	 * 计算用户与用户之间的相似性，返回计算出的用户与用户之间的相似度对象
-	 * @param activeMap 用户对各个二级类目的购买行为的一个map集合
-	 * @return 计算出的用户与用户之间的相似度的对象存储形式
-	 */
-	public static List<UserSimilarityDTO> calcSimilarityBetweenUsers(ConcurrentHashMap<Long, ConcurrentHashMap<Long, Long>> activeMap) {
-		// 用户之间的相似度对集合
-		List<UserSimilarityDTO> similarityList = new ArrayList<UserSimilarityDTO>();
-		
-		// 获取所有的键的集合
-		Set<Long> userSet = activeMap.keySet();
-		// 把这些集合放入ArrayList中
-		List<Long> userIdList = new ArrayList<Long>(userSet);
-		// 小于两个说明当前map集合中只有一个map集合的购买行为，或者一个都没有，直接返回
-		if (userIdList.size() < 2) {
-			return similarityList;
-		}
-		
-		// 计算所有的用户之间的相似度对
-		for (int i = 0; i < userIdList.size() - 1; i++) {
-			for (int j = i + 1; j < userIdList.size(); j++) {
-				// 分别获取两个用户对每个二级类目的点击量
-				ConcurrentHashMap<Long, Long> userCategory2Map = activeMap.get(userIdList.get(i));
-				ConcurrentHashMap<Long, Long> userRefCategory2Map = activeMap.get(userIdList.get(j));
-				
-				// 获取两个map中二级类目id的集合
-				Set<Long> key1Set = userCategory2Map.keySet();
-				Set<Long> key2Set = userRefCategory2Map.keySet();
-				Iterator<Long> it1 = key1Set.iterator();
-				Iterator<Long> it2 = key2Set.iterator();
-				
-				// 两用户之间的相似度
-				double similarity = 0.0;
-				// 余弦相似度公式中的分子
-				double molecule = 0.0;
-				// 余弦相似度公式中的分母
-				double denominator = 1.0;
-				// 余弦相似度公式中分母根号下的两个向量的模的值
-				double vector1 = 0.0;
-				double vector2 = 0.0;
-				
-				while (it1.hasNext() && it2.hasNext()) {
-					Long it1Id = it1.next();
-					Long it2Id = it2.next();
-					// 获取二级类目对应的点击次数
-					Long hits1 = userCategory2Map.get(it1Id);
-					Long hits2 = userRefCategory2Map.get(it2Id);
-					// 累加分子
-					molecule += hits1 * hits2;
-					// 累加分母中的两个向量的模
-					vector1 += Math.pow(hits1, 2);
-					vector2 += Math.pow(hits2, 2);
-				}
-				// 计算分母
-				denominator = Math.sqrt(vector1) * Math.sqrt(vector2);
-				// 计算整体相似度
-				similarity = molecule / denominator;
-				
-				// 创建用户相似度对对象
-				UserSimilarityDTO userSimilarityDTO = new UserSimilarityDTO();
-				userSimilarityDTO.setUserId(userIdList.get(i));
-				userSimilarityDTO.setUserRefId(userIdList.get(j));
-				userSimilarityDTO.setSimilarity(similarity);
-				// 将计算出的用户以及用户之间的相似度对象存入list集合
-				similarityList.add(userSimilarityDTO);
-			}
-		}
-		
-		return similarityList;
-	}
-	
-	/**
-	 * 获取某个用户与其他用户之间的相似度列表,只返回相似度最高的topN个
-	 * @param userId 用户id
-	 * @return userId与其他用户之间的相似度集合
-	 */
-	public static List<Long> getSimilarityBetweenUsers(Long userId, Integer topN) {
-		List<Long> similarityList = new ArrayList<Long>();
-		
-		
-		
-		return similarityList;
-	}
-	
-	/**
-	 * 从与userId相似的用户集合similarUserList中找出要推荐的商品
-	 * @param userId 被推荐商品的用户id
-	 * @param similarUserList 用userId相似的用户集合
-	 * @return 可以推荐给userId的商品列表
-	 */
-	public static List<Product> getRecommendateCategory2(Long userId, List<Long> similarUserList) {
-		List<Product> recommeddateProductList = new ArrayList<Product>();
-		
-		
-		
-		return recommeddateProductList;
-	}
-	
-	/**
-	 * 找出当二级类目下点击量最高的前n个商品
-	 * @param category2Id
-	 * @return
-	 */
-	public static List<Product> findTopNProducts(Long category2Id) {
-		List<Product> recommeddateProductList = new ArrayList<Product>();
-		
-		
-		return recommeddateProductList;
-	}
-	
-	
+    
+    /**
+     * 更新用户的购买行为，最终插入数据库中
+     * @param userId 用户id
+     * @param itemId 商品id
+     * @return true则更新成功，false则更新失败
+     */
+    public static boolean updateBuyingBehavior(Long userId, Long itemId) {
+        boolean flag = false;
+        
+        
+        return flag;
+    }
+    
+    /**
+     * 将用户的购买行为组装成一个map,key为userId，value也是一个map，这个map记录的是二级类目以及它对应的点击量
+     * @param userActiveList 用户的购买行为列表
+     * @return 组装好的用户的购买行为的map集合
+     */
+    public static ConcurrentHashMap<Long, ConcurrentHashMap<Long, Long>> assembleUserBehavior(List<UserActiveDTO> userActiveList) {
+        ConcurrentHashMap<Long, ConcurrentHashMap<Long, Long>> activeMap = new ConcurrentHashMap<Long, ConcurrentHashMap<Long, Long>>();
+        // 遍历查询到的用户点击行为数据
+        for (UserActiveDTO userActive : userActiveList) {
+            // 1.获取用户id
+            Long userId = userActive.getUserId();
+            // 2.获取二级类目id
+            Long category2Id = userActive.getCategory2Id();
+            // 3.获取该二级类目的点击量
+            Long hits = userActive.getHits();
+            
+            // 判断activeMap中是否已经存在了该userId的信息，如果存在则进行更新
+            if (activeMap.containsKey(userId)) {
+                ConcurrentHashMap<Long, Long> tempMap = activeMap.get(userId);
+                tempMap.put(category2Id, hits);
+                activeMap.put(userId, tempMap);
+            } else {
+                // 不存在则直接put进
+                ConcurrentHashMap<Long, Long> category2Map = new ConcurrentHashMap<Long, Long>();
+                category2Map.put(category2Id, hits);
+                activeMap.put(userId, category2Map);
+            }
+        }
+        
+        return activeMap;
+    }
+    
+    /**
+     * 计算用户与用户之间的相似性，返回计算出的用户与用户之间的相似度对象
+     * @param activeMap 用户对各个二级类目的购买行为的一个map集合
+     * @return 计算出的用户与用户之间的相似度的对象存储形式
+     */
+    public static List<UserSimilarityDTO> calcSimilarityBetweenUsers(ConcurrentHashMap<Long, ConcurrentHashMap<Long, Long>> activeMap) {
+        // 用户之间的相似度对集合
+        List<UserSimilarityDTO> similarityList = new ArrayList<UserSimilarityDTO>();
+        
+        // 获取所有的键的集合
+        Set<Long> userSet = activeMap.keySet();
+        // 把这些集合放入ArrayList中
+        List<Long> userIdList = new ArrayList<Long>(userSet);
+        // 小于两个说明当前map集合中只有一个map集合的购买行为，或者一个都没有，直接返回
+        if (userIdList.size() < 2) {
+            return similarityList;
+        }
+        
+        // 计算所有的用户之间的相似度对
+        for (int i = 0; i < userIdList.size() - 1; i++) {
+            for (int j = i + 1; j < userIdList.size(); j++) {
+                // 分别获取两个用户对每个二级类目的点击量
+                ConcurrentHashMap<Long, Long> userCategory2Map = activeMap.get(userIdList.get(i));
+                ConcurrentHashMap<Long, Long> userRefCategory2Map = activeMap.get(userIdList.get(j));
+                
+                // 获取两个map中二级类目id的集合
+                Set<Long> key1Set = userCategory2Map.keySet();
+                Set<Long> key2Set = userRefCategory2Map.keySet();
+                Iterator<Long> it1 = key1Set.iterator();
+                Iterator<Long> it2 = key2Set.iterator();
+                
+                // 两用户之间的相似度
+                double similarity = 0.0;
+                // 余弦相似度公式中的分子
+                double molecule = 0.0;
+                // 余弦相似度公式中的分母
+                double denominator = 1.0;
+                // 余弦相似度公式中分母根号下的两个向量的模的值
+                double vector1 = 0.0;
+                double vector2 = 0.0;
+                
+                while (it1.hasNext() && it2.hasNext()) {
+                    Long it1Id = it1.next();
+                    Long it2Id = it2.next();
+                    // 获取二级类目对应的点击次数
+                    Long hits1 = userCategory2Map.get(it1Id);
+                    Long hits2 = userRefCategory2Map.get(it2Id);
+                    // 累加分子
+                    molecule += hits1 * hits2;
+                    // 累加分母中的两个向量的模
+                    vector1 += Math.pow(hits1, 2);
+                    vector2 += Math.pow(hits2, 2);
+                }
+                // 计算分母
+                denominator = Math.sqrt(vector1) * Math.sqrt(vector2);
+                // 计算整体相似度
+                similarity = molecule / denominator;
+                
+                // 创建用户相似度对对象
+                UserSimilarityDTO userSimilarityDTO = new UserSimilarityDTO();
+                userSimilarityDTO.setUserId(userIdList.get(i));
+                userSimilarityDTO.setUserRefId(userIdList.get(j));
+                userSimilarityDTO.setSimilarity(similarity);
+                // 将计算出的用户以及用户之间的相似度对象存入list集合
+                similarityList.add(userSimilarityDTO);
+            }
+        }
+        
+        return similarityList;
+    }
+    
+    /**
+     * 获取某个用户与其他用户之间的相似度列表,只返回相似度最高的topN个
+     * @param userId 用户id
+     * @return userId与其他用户之间的相似度集合
+     */
+    public static List<Long> getSimilarityBetweenUsers(Long userId, Integer topN) {
+        List<Long> similarityList = new ArrayList<Long>();
+        
+        
+        
+        return similarityList;
+    }
+    
+    /**
+     * 从与userId相似的用户集合similarUserList中找出要推荐的商品
+     * @param userId 被推荐商品的用户id
+     * @param similarUserList 用userId相似的用户集合
+     * @return 可以推荐给userId的商品列表
+     */
+    public static List<Product> getRecommendateCategory2(Long userId, List<Long> similarUserList) {
+        List<Product> recommeddateProductList = new ArrayList<Product>();
+        
+        
+        
+        return recommeddateProductList;
+    }
+    
+    /**
+     * 找出当二级类目下点击量最高的前n个商品
+     * @param category2Id
+     * @return
+     */
+    public static List<Product> findTopNProducts(Long category2Id) {
+        List<Product> recommeddateProductList = new ArrayList<Product>();
+        
+        
+        return recommeddateProductList;
+    }
+    
+    
 }
