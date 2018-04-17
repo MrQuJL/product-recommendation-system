91982bd1b5bfa813fa4e02ccbca8f2d82a6ee840
MrQuJL <909301740@qq.com>
2018-04-17 18:57:56 +0800

    实现商城服务层接口以及对应的实现类，编写密码加密以及验证商城用户登录的业务逻辑

---
+++ shopping/src/main/java/com/lyu/shopping/sysmanage/service/MemberService.java
@@ -0,0 +1,99 @@
+package com.lyu.shopping.sysmanage.service;
+
+import java.util.List;
+
+import com.github.pagehelper.PageInfo;
+import com.lyu.shopping.common.dto.PageParam;
+import com.lyu.shopping.sysmanage.entity.Member;
+
+/**
+ * 类描述：处理商城会员的服务层接口
+ * 类名称：com.lyu.shopping.sysmanage.service.MemberService
+ * @author 曲健磊
+ * 2018年4月17日.下午6:53:40
+ * @version V1.0
+ */
+public interface MemberService {
+
+	/**
+	 * 统计所有商城会员的数目(未删除的)
+	 * @return
+	 */
+	Long countMember();
+	
+	/**
+	 * 通过商城会员id获取商城会员的详细信息
+	 * @param memberId 商城会员id
+	 * @return
+	 */
+	Member getMemberByMemberId(Long memberId);
+	
+	/**
+	 * 验证用户是否存在，若存在，继续验证密码是否匹配
+	 * @param loginName 用户登录名称
+	 * @param password 密码
+	 * @return
+	 */
+	Member loginMember(String loginName, String password);
+	
+	/**
+	 * 根据用户已知信息分页查询用户列表
+	 * @param member 用户信息
+	 * @param pageParam 分页对象
+	 * @return
+	 */
+	PageInfo<Member> listMember(Member member, PageParam pageParam);
+	
+	/**
+	 * 新增管理严
+	 * @param user 待新增的商城会员
+	 * @return
+	 */
+	boolean saveMember(Member member);
+	
+	/**
+	 * 根据商城会员id删除（逻辑删除）商城会员
+	 * @param memberId 商城会员id
+	 * @return
+	 */
+	boolean removeMember(Long memberId);
+	
+	/**
+	 * 批量删除商城会员
+	 * @param memberIds 商城会员id集合
+	 * @return
+	 */
+	boolean removeMemberBatch(List<Long> memberIds);
+	
+	/**
+	 * 修改商城会员信息（不包括密码）
+	 * @param member 要修改的商城会员
+	 * @return
+	 */
+	boolean updateMember(Member member);
+	
+	/**
+	 * 修改商城会员密码
+	 * @param memberId 商城会员id
+	 * @param newPassword 新密码
+	 * @return 修改是否成功
+	 */
+	boolean updatePassword(Long memberId, String oldPassword,
+		String newPassword, String confirmPassword);
+	
+	/**
+	 * 采用sha1加密算法将密码加密
+	 * @param plainPsd 未经过加密的密码
+	 * @return 加密后的面膜
+	 */
+	String encryptPsd(String plainPsd);
+
+	/**
+	 * 验证密码是否正确
+	 * @param plainPsd 未加密的密码
+	 * @param encryptedPsd 加密之后的密码
+	 * @return 密码是否正确 true 正确 false 错误
+	 */
+	boolean validatePsd(String plainPsd, String encryptedPsd);
+	
+}
---
+++ shopping/src/main/java/com/lyu/shopping/sysmanage/service/impl/MemberServiceImpl.java
@@ -0,0 +1,228 @@
+package com.lyu.shopping.sysmanage.service.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.alibaba.druid.util.StringUtils;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.lyu.shopping.common.dto.PageParam;
+import com.lyu.shopping.sysmanage.entity.Member;
+import com.lyu.shopping.sysmanage.mapper.MemberMapper;
+import com.lyu.shopping.sysmanage.service.MemberService;
+import com.lyu.shopping.util.EncryptUtils;
+
+/**
+ * 类描述：商城会员服务层接口的实现类
+ * 类名称：com.lyu.shopping.sysmanage.service.impl.MemberServiceImpl
+ * @author 曲健磊
+ * 2018年4月17日.下午6:55:06
+ * @version V1.0
+ */
+@Service("memberService")
+public class MemberServiceImpl implements MemberService {
+
+	private static final int HASH_ITERATIONS = 1024;
+	
+	private static final int SALT_SIZE = 8;
+	
+	private static final String DEFAULT_PASSWORD = "123";
+	
+	@Autowired
+	private MemberMapper memberMapper;
+	
+	@Override
+	public Long countMember() {
+		Long memberNums = this.memberMapper.countMember();
+		return memberNums;
+	}
+	
+	@Override
+	public Member getMemberByMemberId(Long memberId) {
+		Member member = this.memberMapper.getMemberByMemberId(memberId);
+		return member;
+	}
+	
+	@Override
+	public Member loginMember(String loginName, String password) {
+		if (StringUtils.isEmpty(loginName) || StringUtils.isEmpty(password)) {
+			return null;
+		}
+		Member member = new Member();
+		member.setLoginName(loginName);
+		
+		// 调用mapper去查询符合条件的用户列表
+		List<Member> userList = this.memberMapper.listMember(member);
+		if (userList.isEmpty()) {
+			return null;
+		}
+		
+		// 用户列表不为空则验证密码是否正确，正确则返回该用户
+		Member plainUser = userList.get(0);
+		String encryptedPassword = plainUser.getPassword();
+		if (validatePsd(password, encryptedPassword)) {
+			return plainUser;
+		}
+		
+		return null;
+	}
+
+	@Override
+	public PageInfo<Member> listMember(Member member, PageParam pageParam) {
+		PageHelper.startPage(pageParam.getPageNo(), pageParam.getPageSize());
+		
+		List<Member> memberList = this.memberMapper.listMember(member);
+		
+		PageInfo<Member> pageInfo = new PageInfo<Member>(memberList);
+		
+		return pageInfo;
+	}
+	
+	@Override
+	public boolean saveMember(Member user) {
+		if (user == null) return false;
+		user.setPassword(this.encryptPsd(DEFAULT_PASSWORD));
+		user.setStatus(0);
+		user.setGmtCreate(new Date());
+		user.setGmtModified(new Date());
+		user.setDelFlag(0);
+		
+		int rows = this.memberMapper.saveMember(user);
+		if (rows > 0) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	@Override
+	public boolean removeMember(Long memberId) {
+		int rows = this.memberMapper.removeMember(memberId);
+		if (rows > 0) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	@Override
+	@Transactional(isolation=Isolation.DEFAULT, propagation = Propagation.REQUIRED)
+	public boolean removeMemberBatch(List<Long> memberIds) {
+		if (memberIds == null || memberIds.size() == 0) {
+			return false;
+		}
+		// 先检查集合中的memberId是否存在
+		int existMemberNums = this.memberMapper.countMemberInList(memberIds);
+		if (existMemberNums != memberIds.size()) {
+			return false;
+		}
+		
+		// 批量删除
+		int removeSuccessNums = this.memberMapper.removeMemberBatch(memberIds);
+		
+		if (removeSuccessNums == existMemberNums) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	@Override
+	public boolean updateMember(Member member) {
+		if (member == null) return false;
+		if (member.getAge() == null || member.getAge() > 999 || member.getAge() < 1) {
+			return false;
+		}
+		if (member.getMobile() == "" || !member.getMobile().matches("1[0-9]{10}") ||
+			member.getMobile().length() > 11) {
+			return false;
+		}
+		
+		// 设置商城会员的修改时间
+		member.setGmtModified(new Date());
+		int rows = this.memberMapper.updateMember(member);
+		if (rows > 0) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	@Override
+	@Transactional(isolation=Isolation.DEFAULT, propagation = Propagation.REQUIRED)
+	public boolean updatePassword(Long memberId, String oldPassword,
+			String newPassword, String confirmPassword) {
+		// 1.确定密码用户id是否为空
+		if (StringUtils.isEmpty(oldPassword) || StringUtils.isEmpty(newPassword) || 
+			StringUtils.isEmpty(confirmPassword) || memberId == null) {
+			return false; 
+		}
+		
+		// 2.确定旧密码是否正确
+		String dbPassword = this.memberMapper.getMemberByMemberId(memberId).getPassword();
+		boolean flag = this.validatePsd(oldPassword, dbPassword);
+		if (!flag) {
+			return false;
+		}
+		
+		// 3.确定两次输入的新密码是否一致
+		if (!newPassword.equals(confirmPassword)) {
+			return false;
+		}
+		
+		// 将密码加下密
+		String encryptPassword = encryptPsd(newPassword);
+		int rows = this.memberMapper.updatePassword(memberId, encryptPassword);
+		if (rows > 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public String encryptPsd(String plainPsd) {
+		// 1.获取随机数
+		byte[] salt = EncryptUtils.generateSalt(SALT_SIZE);
+		// 2.对随机数用Hex编码
+		String encodedSalt = EncryptUtils.encodeHex(salt);
+		// 3.将随机数和盐通过sha1算法加密
+		byte[] sha1Psd = EncryptUtils.sha1(plainPsd.getBytes(), salt, HASH_ITERATIONS);
+		// 4.将上一步得到的值通过Hex来编码
+		String hexSha1 = new String(Hex.encodeHex(sha1Psd));
+		// 5.将第二步和第四步的值拼凑
+		String encryptedPsd = encodedSalt + hexSha1;
+		
+		return encryptedPsd;
+	}
+
+	@Override
+	public boolean validatePsd(String plainPsd, String encryptedPsd) {
+		boolean flag = false;
+		try {
+			// 1.获取密文中的盐
+			byte[] salt = Hex.decodeHex(encryptedPsd.substring(0, SALT_SIZE * 2).toCharArray());
+			// 2.将盐和plainPsd加密HASH_ITERATIONS次
+			byte[] sha1Psd = EncryptUtils.sha1(plainPsd.getBytes(), salt, HASH_ITERATIONS);
+			// 3.用Hex来编码
+			String sha1Hex = new String(Hex.encodeHex(sha1Psd));
+			// 4.拼凑第三步得到的值和密文中的盐
+			String psd = encryptedPsd.substring(0, SALT_SIZE * 2) + sha1Hex;
+			if (psd.equals(encryptedPsd)) {
+				flag = true;
+			}
+		} catch (DecoderException e) {
+			e.printStackTrace();
+		}
+		
+		return flag;
+	}
+
+}
