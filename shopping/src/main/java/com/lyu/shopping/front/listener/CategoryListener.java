fa190f2f046d38c91780790cd11fd23d5cd346f2
MrQuJL <909301740@qq.com>
2018-04-24 23:05:08 +0800

    解决了在监听器中无法注入 Spring 的ApplicationContext 的问题
    原因：想要注入 ApplicationContext 上下文，首先要将监听器注册到 Spring 的上下文中。
    解决：加上@Service注解，进行注册

--- shopping/src/main/java/com/lyu/shopping/front/listener/CategoryListener.java
+++ shopping/src/main/java/com/lyu/shopping/front/listener/CategoryListener.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpSessionListener;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Service;
 
 import com.lyu.shopping.sysmanage.dto.Category1DTO;
 import com.lyu.shopping.sysmanage.entity.Category1;
@@ -22,17 +23,18 @@ import com.lyu.shopping.sysmanage.service.Category1Service;
  * 2018年4月23日.下午9:19:01
  * @version V1.0
  */
+@Service
 @WebListener
 public class CategoryListener implements HttpSessionListener, ApplicationContextAware {
 
-	private ApplicationContext applicationContext;
+	private static ApplicationContext applicationContext;
 	
 	/**
 	 * session被创建的时候，将一二级类目列表加载到内存
 	 */
     public void sessionCreated(HttpSessionEvent se)  { 
     	// 1.从IOC容器中获取一级类目服务类
-    	Category1Service category1Service = (Category1Service) this.applicationContext.getBean("category1Service");
+    	Category1Service category1Service = (Category1Service) CategoryListener.applicationContext.getBean("category1Service");
     	// 2.设置一些查询条件
     	Category1 category1 = new Category1();
 		category1.setShowFlag(1);
