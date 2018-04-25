93375c9c6945d20fc77e85311b037fb7aa1f850c
MrQuJL <909301740@qq.com>
2018-04-26 00:10:40 +0800

    replace tab with for blank on RecommendUtils

--- shopping/src/main/java/com/lyu/shopping/recommendate/util/RecommendUtils.java
+++ shopping/src/main/java/com/lyu/shopping/recommendate/util/RecommendUtils.java
@@ -146,34 +146,34 @@ public class RecommendUtils {
      * @return 与usereId最相似的topN个用户
      */
     public static List<Long> getSimilarityBetweenUsers(Long userId, List<UserSimilarityDTO> userSimilarityDTOList, Integer topN) {
-    	// 用来记录与userId相似度最高的前N个用户的id
+        // 用来记录与userId相似度最高的前N个用户的id
         List<Long> similarityList = new ArrayList<Long>(topN);
         // 堆排序找出最高的前N个用户，建立小根堆，遍历的时候当前的这个相似度比堆顶元素大就剔掉堆顶的值，把这个数入堆(把小的都删除干净,所以要建立小根堆)
         PriorityQueue<UserSimilarityDTO> minHeap = new PriorityQueue<UserSimilarityDTO>(new Comparator<UserSimilarityDTO>(){
-			@Override
-			public int compare(UserSimilarityDTO o1, UserSimilarityDTO o2) {
-				if (o1.getSimilarity() - o2.getSimilarity() > 0) {
-					return 1;
-				} else if (o1.getSimilarity() - o2.getSimilarity() == 0) {
-					return 0;
-				} else {
-					return -1;
-				}
-			}
+            @Override
+            public int compare(UserSimilarityDTO o1, UserSimilarityDTO o2) {
+                if (o1.getSimilarity() - o2.getSimilarity() > 0) {
+                    return 1;
+                } else if (o1.getSimilarity() - o2.getSimilarity() == 0) {
+                    return 0;
+                } else {
+                    return -1;
+                }
+            }
         });
         
         for (UserSimilarityDTO userSimilarityDTO : userSimilarityDTOList) {
-        	if (minHeap.size() < topN) {
-        		minHeap.offer(userSimilarityDTO);
-        		System.out.println(minHeap.peek().getSimilarity());
-        	} else if (minHeap.peek().getSimilarity() < userSimilarityDTO.getSimilarity()) {
-        		minHeap.poll();
-        		minHeap.offer(userSimilarityDTO);
-        	}
+            if (minHeap.size() < topN) {
+                minHeap.offer(userSimilarityDTO);
+                System.out.println(minHeap.peek().getSimilarity());
+            } else if (minHeap.peek().getSimilarity() < userSimilarityDTO.getSimilarity()) {
+                minHeap.poll();
+                minHeap.offer(userSimilarityDTO);
+            }
         }
         // 把得到的最大的相似度的用户的id取出来(不要取它自己)
         for (UserSimilarityDTO userSimilarityDTO : minHeap) {
-        	similarityList.add(userSimilarityDTO.getUserId() == userId ? userSimilarityDTO.getUserRefId() : userSimilarityDTO.getUserId());
+            similarityList.add(userSimilarityDTO.getUserId() == userId ? userSimilarityDTO.getUserRefId() : userSimilarityDTO.getUserId());
         }
         
         return similarityList;
@@ -193,37 +193,37 @@ public class RecommendUtils {
         List<UserActiveDTO> userIdActiveList = findUsersBrowsBehavior(userId, userActiveList);
         // 对userId的浏览行为按照二级类目id排个序，方便后续与推荐的用户的浏览行为中的二级类目的点击次数直接相减，避免时间复杂度为O(n2)
         Collections.sort(userIdActiveList, new Comparator<UserActiveDTO>(){
-			@Override
-			public int compare(UserActiveDTO o1, UserActiveDTO o2) {
-				return o1.getCategory2Id().compareTo(o2.getCategory2Id());
-			}
-		});
+            @Override
+            public int compare(UserActiveDTO o1, UserActiveDTO o2) {
+                return o1.getCategory2Id().compareTo(o2.getCategory2Id());
+            }
+        });
 
         // 1.从与useId浏览行为相似的每个用户中找出一个推荐的二级类目
         for (Long refId : similarUserList) {
-        	// 计算当前用户所点击的二级类目次数与被推荐的用户所点击的二级类目的次数的差值
-        	// 找到当前这个用户的浏览行为
-        	List<UserActiveDTO> currActiveList = findUsersBrowsBehavior(refId, userActiveList);
-        	// 排序，同上述理由
-        	Collections.sort(currActiveList, new Comparator<UserActiveDTO>(){
-    			@Override
-    			public int compare(UserActiveDTO o1, UserActiveDTO o2) {
-    				return o1.getCategory2Id().compareTo(o2.getCategory2Id());
-    			}
-    		});
-        	// 记录差值最大的二级类目的id
-        	long maxCategory2 = 0L;
-        	// 记录最大的差值
-        	double maxDifference = 0.0;
-        	for (int i = 0; i < currActiveList.size(); i++) {
-        		// 求出点击量差值最大的二级类目，即为要推荐的类目
-        		double difference = Math.abs(currActiveList.get(i).getHits() - userIdActiveList.get(i).getHits());
-        		if (difference > maxDifference) {
-        			maxDifference = difference;
-        			maxCategory2 = currActiveList.get(i).getCategory2Id();
-        		}
-        	}
-        	recommeddateProductList.add(maxCategory2);
+            // 计算当前用户所点击的二级类目次数与被推荐的用户所点击的二级类目的次数的差值
+            // 找到当前这个用户的浏览行为
+            List<UserActiveDTO> currActiveList = findUsersBrowsBehavior(refId, userActiveList);
+            // 排序，同上述理由
+            Collections.sort(currActiveList, new Comparator<UserActiveDTO>(){
+                @Override
+                public int compare(UserActiveDTO o1, UserActiveDTO o2) {
+                    return o1.getCategory2Id().compareTo(o2.getCategory2Id());
+                }
+            });
+            // 记录差值最大的二级类目的id
+            long maxCategory2 = 0L;
+            // 记录最大的差值
+            double maxDifference = 0.0;
+            for (int i = 0; i < currActiveList.size(); i++) {
+                // 求出点击量差值最大的二级类目，即为要推荐的类目
+                double difference = Math.abs(currActiveList.get(i).getHits() - userIdActiveList.get(i).getHits());
+                if (difference > maxDifference) {
+                    maxDifference = difference;
+                    maxCategory2 = currActiveList.get(i).getCategory2Id();
+                }
+            }
+            recommeddateProductList.add(maxCategory2);
         }
         return recommeddateProductList;
     }
@@ -235,13 +235,13 @@ public class RecommendUtils {
      * @return 当前用户的浏览行为列表
      */
     public static List<UserActiveDTO> findUsersBrowsBehavior(Long userId, List<UserActiveDTO> userActiveList) {
-    	List<UserActiveDTO> currActiveList = new ArrayList<UserActiveDTO>();
-    	for (UserActiveDTO userActiveDTO : userActiveList) {
-    		if (userActiveDTO.getUserId().equals(userId)) {
-    			currActiveList.add(userActiveDTO);
-    		}
-    	}
-    	return currActiveList;
+        List<UserActiveDTO> currActiveList = new ArrayList<UserActiveDTO>();
+        for (UserActiveDTO userActiveDTO : userActiveList) {
+            if (userActiveDTO.getUserId().equals(userId)) {
+                currActiveList.add(userActiveDTO);
+            }
+        }
+        return currActiveList;
     }
 
     /**
@@ -250,18 +250,18 @@ public class RecommendUtils {
      * @return 点击量最高的商品
      */
     public static Product findMaxHitsProduct(List<? extends Product> productList) {
-    	if (productList == null || productList.size() == 0) {
-    		return null;
-    	}
-    	// 记录当前最大的点击量
-    	Long maxHits = 0L;
-    	// 记录当前点击量最大的商品
-    	Product product = null;
+        if (productList == null || productList.size() == 0) {
+            return null;
+        }
+        // 记录当前最大的点击量
+        Long maxHits = 0L;
+        // 记录当前点击量最大的商品
+        Product product = null;
         for (Product temp : productList) {
-        	if (temp.getHits() >= maxHits) {
-        		maxHits = temp.getHits(); 
-        		product = temp;
-        	}
+            if (temp.getHits() >= maxHits) {
+                maxHits = temp.getHits(); 
+                product = temp;
+            }
         }
         return product;
     }
